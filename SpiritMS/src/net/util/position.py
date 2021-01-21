91a0f8d23decba20e2e1d3a69dea162a131ebee9
Bratah123 <brandonnguyen3301@gmail.com>
2021-01-20 16:28:16 -0800

    Position struct getters and setters

---
+++ SpiritMS/src/net/util/position.py
@@ -0,0 +1,20 @@
+class Position:
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, x_pos):
+        self._x = x_pos
+
+    @property
+    def y(self):
+        return self._y
+    
+    @y.setter
+    def y(self, y_pos):
+        self._y = y_pos
