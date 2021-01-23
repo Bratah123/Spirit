9cd26f7baf7bdae49529ca894e82df00ce355793
Bratah123 <brandonnguyen3301@gmail.com>
2021-01-23 12:20:40 -0800

    white space and doc fixes

--- SpiritMS/src/net/client/character/character.py
+++ SpiritMS/src/net/client/character/character.py
@@ -14,6 +14,7 @@ class Character:
             You guessed it, this attribute stores all of the characters cosmetic related stats.
             (I.E. HAIR, FACE, EYE)
     """
+
     def __init__(
             self,
             chr_id=0,
