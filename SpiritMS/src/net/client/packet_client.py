cdea20ac69b7e6f81d18e73b8e99f5186e33ae96
Bratah123 <brandonnguyen3301@gmail.com>
2021-01-26 13:19:36 -0800

    removed unnecessary imports

--- SpiritMS/src/net/client/packet_client.py
+++ SpiritMS/src/net/client/packet_client.py
@@ -1,6 +1,5 @@
 from src.net.debug import debug
 from src.net.packets.byte_buffer.packet import Packet
-from src.net.packets.send_ops import OutPacket
 from src.net.server import server_constants
 
 
